2338113204e0eabf2faef217aec6527fb8a72961
wwog <871782513@qq.com>
2023-09-11 11:37:20 +0800

    ✨ feat(sleep):

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { promiseRetry } from './promiseRetry'
 import { promiseTimeout } from './promiseTimeout'
 import { promiseTry } from './promiseTry'
+import { sleep } from './sleep'
 export type * from './types'
 
 export class PromiseEx<T = unknown> extends Promise<T> {
   static retry = promiseRetry
   static timeout = promiseTimeout
   static try = promiseTry
+  static sleep = sleep
 
   private constructor(noNew: any) {
     super(noNew)
